fef5e27621a18201ec48ae1c373a6a70d1751ae0
Anthony Poschen <zanven42@gmail.com>
2023-12-08 20:55:20 +1100

    improve golang formatting to use goreturns and improve doco formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,54 +7,55 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
-  spec = {
-    -- add LazyVim and import its plugins
-    { import = "lazyvim.plugins" },
-    -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.go" },
-    { "laytan/cloak.nvim" },
-    { "tpope/vim-fugitive" },
-    { "jremmen/vim-ripgrep" },
+	spec = {
+		-- add LazyVim and import its plugins
+		{ import = "lazyvim.plugins" },
+		-- import any extras modules here
+		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.lang.json" },
+		{ import = "lazyvim.plugins.extras.lang.go" },
+		{ "laytan/cloak.nvim" },
+		{ "tpope/vim-fugitive" },
+		{ "jremmen/vim-ripgrep" },
+		{ "s1n7ax/nvim-window-picker" },
 
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- import/override with your plugins
-    -- { import = "plugins" },
-  },
-  defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
-  },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+		-- import/override with your plugins
+		-- { import = "plugins" },
+	},
+	defaults = {
+		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+		lazy = false,
+		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+		-- have outdated releases, which may break your Neovim install.
+		version = false, -- always use the latest git commit
+		-- version = "*", -- try installing the latest stable version for plugins that support semver
+	},
+	install = { colorscheme = { "tokyonight", "habamax" } },
+	checker = { enabled = true }, -- automatically check for plugin updates
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
 -- require("lazy").setup("lazyvim.plugins")
 -- vim.opt.rtp:prepend(lazypath)
 -- lazyvim
 -- require('lazyvim').setup()
 -- vim options and autocmds
-require('vim_options')
+require("vim_options")
 -- need to remove any that are for plugins into its after file
 --" learning materials for lua
 --" https://vonheikemen.github.io/devlog/tools/configuring-neovim-using-lua/
@@ -64,4 +65,8 @@ require('vim_options')
 -- vim.g.copilot_assume_mapped = true
 -- vim.g.copilot_tab_fallback = ""
 vim.g.copilot_proxy = vim.env.HTTPS_PROXY
-
+require("conform").setup({
+	formatters_by_ft = {
+		go = { "goreturns" },
+	},
+})
--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Setup
+
 Install brew then run the following to install to brew file
-```
+
+```sh
 brew bundle
 ```
 
 Clone the repo down
-```
+
+```sh
 git clone --bare https://github.com/AnthonyPoschen/dotfiles $HOME/.cfg
 alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config checkout
 ```
+
 Install brew
-```
+
+```sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # Brew might not be in path so might need to fix that / run binary directly
 brew bundle
@@ -20,43 +25,58 @@ brew bundle
 swap to kitty terminal before proceeding.
 
 Install ohmyzsh
-```
+
+```sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 ```
+
 config may be required for oh my zsh
 
 Install tmux plugin manager
-```
+
+```sh
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ```
 
 Install tmux plugins with following hotkey Ctrl+A I
-```
+
+```sh
 ^A + I
 ```
+
 Install zsh zplug plugins
-```
+
+```sh
 zplug install
 ```
 
 Setting up p10k
 install custom fonts before setting up zsh
-```
+
+```sh
 https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 ```
+
 Installing oh-my-zsh plugins
-```
+
+```sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 ```
 
-
 Configuring podman post install
-```
+
+```sh
 podman machine init --cpu 8 --memory=8192
 podman machine ssh sudo rpm-ostree install qemu-user-static
 podman machine ssh sudo systemctl reboot
 ```
+
+Configure golang packages that aren't managed elsewhere
+
+```sh
+go install github.com/sqs/goreturns@latest
+````
