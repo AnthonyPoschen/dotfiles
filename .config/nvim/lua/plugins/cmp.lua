de260033ff471287836f6f1480e98cde67141307
Anthony Poschen <zanven42@gmail.com>
2023-12-13 12:33:49 +1100

    html auto indent on enter resolved. hell yeah

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,52 +1,77 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-emoji",
-  },
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {}, -- this is equalent to setup({}) function
+	},
+	{
 
-    local luasnip = require("luasnip")
-    local cmp = require("cmp")
-    local tab = function(fallback)
-        if cmp.visible() then
-          -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-          cmp.select_next_item()
-        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-        -- this way you will only jump inside the snippet region
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-emoji",
+			"windwp/nvim-autopairs",
+		},
+		---@param opts cmp.ConfigSchema
+		opts = function(_, opts)
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 
-    local stab = function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end
+			local luasnip = require("luasnip")
+			local cmp = require("cmp")
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			local tab = function(fallback)
+				if cmp.visible() then
+					-- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+					cmp.select_next_item()
+				-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+				-- this way you will only jump inside the snippet region
+				elseif luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				elseif has_words_before() then
+					cmp.complete()
+				else
+					fallback()
+				end
+			end
 
+			local stab = function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end
 
-    opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ['<cr>'] = cmp.mapping.confirm({ select = true }),
-      ["<Tab>"] = cmp.mapping(tab, { "i", "s" }),
-      ["<c-j>"] = cmp.mapping(tab, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(stab, { "i", "s" }),
-      ["<c-k>"] = cmp.mapping(stab, { "i", "s" }),
-      ['<c-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-      ['<c-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    })
-  end,
+			opts.mapping = vim.tbl_extend("force", opts.mapping, {
+				["<cr>"] = cmp.mapping.confirm({ select = true }),
+				["<Tab>"] = cmp.mapping(tab, { "i", "s" }),
+				["<c-j>"] = cmp.mapping(tab, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(stab, { "i", "s" }),
+				["<c-k>"] = cmp.mapping(stab, { "i", "s" }),
+				["<c-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+				["<c-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+			})
+		end,
+	},
 }
+-- return {
+--   {
+--     "hrsh7th/nvim-cmp",
+--     dependencies = {
+--       "windwp/nvim-autopairs",
+--       opts = {},
+--     },
+--     opts = function()
+--       local cmp = require("cmp")
+--       local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+--       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+--     end,
+--   },
+-- }
