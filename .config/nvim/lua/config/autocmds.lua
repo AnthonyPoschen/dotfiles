18f760c3ec726c79b3b12e000cf8aad42b42b398
Anthony Poschen <zanven42@gmail.com>
2023-12-31 04:18:33 +1100

    some more work to config. TODO: fix up commits and other telescope viewers

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -4,7 +4,7 @@
 
 -- fix kitty terminal font map when leaving vim
 -- vim.cmd("autocmd VimLeave <cmd>lua os.execute(\"kitty @ --to unix:/tmp/kitty ret-font-size '0'\")<CR>")
-vim.api.nvim_create_autocmd({ "VimLeave" }, {
+vim.api.nvim_create_autocmd({ "VimLeave", "VimLeavePre" }, {
 	callback = function(ev, opts)
 		os.execute("kitty @ --to unix:/tmp/kitty set-font-size '0'")
 	end,
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -21,17 +21,17 @@ return {
 				-- find
 				--
 				{ "<leader>f", Util.telescope("files"), desc = "Find files" },
-				{ "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
-				{ "<leader>fc", Util.telescope.config_files(), desc = "Find Config File" },
-				{ "<leader>ff", Util.telescope("files"), desc = "Find Files (root dir)" },
-				{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-				{ "<leader>fF", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
-				{ "<leader>fR", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)" },
+				-- { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
+				-- { "<leader>fc", Util.telescope.config_files(), desc = "Find Config File" }, -- this opens file finder of just vim config files
+				-- { "<leader>ff", Util.telescope("files"), desc = "Find Files (root dir)" },
+				{ "<leader>r", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+				-- { "<leader>fF", Util.telescope("files", { cwd = false }), desc = "Fjnd Files (cwd)" },
+				-- { "<leader>fR", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)" },
 				-- git
+				-- { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
 				{ "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
 				-- { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "status" },
 				-- search
-				{ '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Registers" },
 				{ "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
 				{ "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
 				{ "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
@@ -43,10 +43,11 @@ return {
 				{ "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
 				{ "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
 				{ "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
-				{ "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
-				{ "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
+				{ "<leader>sm", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+				{ "<leader>sM", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
 				{ "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-				{ "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
+				{ '<leader>sr"', "<cmd>Telescope registers<cr>", desc = "Registers" },
+				-- { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
 				{ "<leader>sw", Util.telescope("grep_string", { word_match = "-w" }), desc = "Word (root dir)" },
 				{
 					"<leader>sW",
@@ -124,6 +125,22 @@ return {
 				Util.telescope("find_files", { hidden = true, default_text = line })()
 			end
 
+			local config_result_prompt = {
+				-- theme = "dropdown",
+				hidden = true,
+				previewer = false,
+				layout_strategy = "vertical",
+				sorting_strategy = "descending",
+				layout_config = {
+					prompt_position = "bottom",
+					width = 130,
+					height = 0.75,
+				},
+				show_line = false,
+				results_title = false,
+				-- preview_title = false,
+			}
+
 			return {
 				extensions = {
 					fzf = {
@@ -150,23 +167,17 @@ return {
 					-- },
 				},
 				pickers = {
-					find_files = {
-						-- theme = "dropdown",
-						hidden = true,
-						previewer = false,
-						layout_strategy = "vertical",
-						sorting_strategy = "descending",
-						layout_config = {
-							prompt_position = "bottom",
-							width = 130,
-							height = 0.75,
+					find_files = config_result_prompt,
+					oldfiles = config_result_prompt,
+					command_history = config_result_prompt,
+					commands = config_result_prompt,
+					diagnostics = config_result_prompt,
+					git_commits = {
+						git_command = {
+							"git",
+							"log",
+							"-i --pretty=format:'%<(5)%C(auto,yellow)%h %ad %C(auto,blue)%><(20)[ %cn ] %Creset%-s %C(dim blue)(%ar)%-C(auto,bold red)%-d' --decorate --date=short\" -",
 						},
-						show_line = false,
-						results_title = false,
-						-- preview_title = false,
-					},
-					file_browser = {
-						hidden = true,
 					},
 				},
 				defaults = {
