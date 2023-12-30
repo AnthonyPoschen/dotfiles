ce52e7f3eca74c0381b4c6beec44c4afc0516e6d
Anthony Poschen <zanven42@gmail.com>
2023-12-30 14:32:37 +1100

    ensure when nvim closed in zen mode, terminal resets correctly

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- fix kitty terminal font map when leaving vim
+-- vim.cmd("autocmd VimLeave <cmd>lua os.execute(\"kitty @ --to unix:/tmp/kitty ret-font-size '0'\")<CR>")
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+	callback = function(ev, opts)
+		os.execute("kitty @ --to unix:/tmp/kitty set-font-size '0'")
+	end,
+})
