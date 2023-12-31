c0ae59496e160b9f57c87fc8002441c03fc49065
Anthony Poschen <zanven42@gmail.com>
2023-12-31 14:25:40 +1100

    resolved all zenmode issues with tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -73,3 +73,6 @@ require("lazy").setup({
 -- vim.g.copilot_assume_mapped = true
 -- vim.g.copilot_tab_fallback = ""
 vim.g.copilot_proxy = vim.env.HTTPS_PROXY
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_tab_fallback = ""
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,6 +6,8 @@
 -- vim.cmd("autocmd VimLeave <cmd>lua os.execute(\"kitty @ --to unix:/tmp/kitty ret-font-size '0'\")<CR>")
 vim.api.nvim_create_autocmd({ "VimLeave", "VimLeavePre" }, {
 	callback = function(ev, opts)
-		os.execute("kitty @ --to unix:/tmp/kitty set-font-size '0'")
+		os.execute("kitty @ --to $KITTY_LISTEN_ON set-font-size '0'")
+		os.execute("tmux set status 2")
+		os.execute("tmux list-panes -F '\\#F' | grep -q Z && tmux resize-pane -Z")
 	end,
 })
