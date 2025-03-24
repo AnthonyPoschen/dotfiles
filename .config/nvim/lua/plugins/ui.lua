d861eaaf45c6cd168d8e38f74d6ef782325e13d2
Anthony Poschen <zanven42@gmail.com>
2025-03-24 14:51:04 +1100

    feat: remove unused lua function

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -264,40 +264,6 @@ return {
 					color = opts.color,
 				}
 			end
-			local function pretty_path(opts)
-				opts = vim.tbl_extend("force", {
-					relative = "cwd",
-					modified_hl = "Constant",
-				}, opts or {})
-
-				return function(self)
-					local path = vim.fn.expand("%:p") --[[@as string]]
-
-					if path == "" then
-						return ""
-					end
-					local root = getroot({ normalize = true })
-					local cwd = cwdf()
-
-					if opts.relative == "cwd" and path:find(cwd, 1, true) == 1 then
-						path = path:sub(#cwd + 2)
-					else
-						path = path:sub(#root + 2)
-					end
-
-					local sep = package.config:sub(1, 1)
-					local parts = vim.split(path, "[\\/]")
-					if #parts > 3 then
-						parts = { parts[1], "…", parts[#parts - 1], parts[#parts] }
-					end
-
-					if opts.modified_hl and vim.bo.modified then
-						parts[#parts] = M.format(self, parts[#parts], opts.modified_hl)
-					end
-
-					return table.concat(parts, sep)
-				end
-			end
 			return {
 				options = {
 					theme = "auto",
